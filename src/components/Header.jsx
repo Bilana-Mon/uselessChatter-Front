bbaf4ecc83d0a74610f785a3d92b81ee4838edb7
Bilana-Mon <bilanamoon@gmail.com>
2022-06-06 13:15:27 +0300

    added header cmp and logo img

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <header className="flex justify-between">
+            <div className="logo">
+                <div><img src="./assets/icons/useless-logo.png" alt="Sloth icon by Icons8" /></div>
+            <div><span>useless</span><span>Chatter</span></div>
+            </div>
+            <nav>
+                <ul>
+                    <li>some</li>
+                    <li>thing</li>
+                    <li>rrrr</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'useless':'#FEE9D0',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
